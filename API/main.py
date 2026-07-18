34dafdc62cc32919becc5c882cec88b567c55317
PlaySmith <161820504+PlaySmith@users.noreply.github.com>
2026-07-18 20:36:39 +0530

    Post Heartbeat simple logic

--- API/main.py
+++ API/main.py
@@ -1,2 +1,22 @@
-# testing Docker 
-# https://prod.liveshare.vsengsaas.visualstudio.com/join?613B10C7BB3EBC4FA1F190A1B4284466E2DF
+from fastapi import FastAPI
+from datetime import datetime
+from pydantic import BaseModel
+
+api = FastAPI()
+
+@api.get("/")
+def home():
+    return {"message": "Video Streaming Analytics API is running"}
+
+class Heartbeat(BaseModel):
+    user_id: int
+    video_id: int
+    watch_duration: int
+    timestamp: datetime
+    
+@api.post("/heartbeat")
+def receive_heartbeat(data: Heartbeat):
+    return{
+        "message": "Heartbeat received successfully",
+        "data": data
+    }
