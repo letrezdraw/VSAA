30ab0599f33528a7503f37dca40d88f3ca7245fe
PlaySmith <161820504+PlaySmith@users.noreply.github.com>
2026-07-20 23:59:34 +0530

    Producer added

---
+++ Queue/Producer.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+import _json
+def publish(heartbeat):
+    """
+    Publish object from the API like Heartbeat to the Queue.
+    """
+    print(f"Publishing heartbeat of usser {heartbeat.user_id}")
+    
+    heartbeat_dict = heartbeat.model_dump() #convets object to dict
+    print(f"Heartbeat data: {heartbeat_dict}")
+     
+    heartbeat_json = _json.dumps(heartbeat_dict) #convets the dict to json 
+    print(f"Heartbeat JSON: {heartbeat_json}") 
