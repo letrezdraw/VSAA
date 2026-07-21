fa73fe06c2d33fe22c06969e8363c44cec85fe62
letrezdraw <letrezdraw@gmail.com>
2026-07-21 18:52:47 +0530

    well did somethinn

--- consumer/worker.py
+++ consumer/worker.py
@@ -1,1 +1,17 @@
-# worker.py file as of now
+from quixstreams import Application
+
+app = Application(
+    broker_address="localhost:9092",
+    loglevel="DEBUG",
+    consumer_group="Heartbeat_Consumer",
+)
+
+topic = app.topic("video-heartbeat", value_deserializer="json")
+
+sdf = app.dataframe(topic)
+
+sdf.print()
+
+if __name__ == "__main__":
+    print("Starting Consumer Worker")
+    app.run(sdf)
